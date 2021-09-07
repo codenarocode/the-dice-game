a9d7bfc8286ee03ca3694415c1137334a1a37d1f
Narottam Narhari <87809175+codenarocode@users.noreply.github.com>
2021-09-07 20:07:29 +0530

    Add files via upload

---
+++ functioning/dicee.js
@@ -0,0 +1,21 @@
+
+let random1= Math.floor(6*Math.random())+1;
+
+document.querySelector(".img1").setAttribute("src","images/dice" +random1 +".png");
+
+
+let random2=Math.floor(6*Math.random())+1;
+document.querySelector(".img2").setAttribute("src","images/dice" +random2 +".png");
+
+
+if(random1==random2){
+  document.querySelector("h1").innerHTML="🎈Match Draw!🎈";
+}
+
+else if(random1>random2){
+    document.querySelector("h1").innerHTML="🚩Alpha Wins!🚩";
+}
+else{
+  document.querySelector("h1").innerHTML="🚩Sigma Wins!🚩";
+
+}
